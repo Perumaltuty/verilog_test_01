85b71f83adcf27607ed0e5f24f339a28a82719c3
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-26 23:55:29 +0530

    Create tb.v

---
+++ prob_6/tb.v
@@ -0,0 +1,21 @@
+
+module tb;
+  reg d,rst;
+  reg clk=0;
+  wire out,q;
+d_ff dut( .d(d),.clk(clk),.rst(rst),.out(out),.q(q)); 
+   always #5 clk = ~clk;
+   initial begin
+     rst=0;
+       d=1;
+     #5 rst=1;
+     #5 d=1;
+     #5 d=1;
+     #5 rst =0;
+     #5 d=0;
+     #5 d=1;
+     #5 rst=1;
+   end
+ endmodule
+ 
+     
