6ce0a5bdfe5c15210ea19026d7e4f52d72e15d3a
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-27 00:08:44 +0530

    Create 7_1.v

---
+++ prob_7/7_1.v
@@ -0,0 +1,18 @@
+
+
+module d_flipflop( input d,input clk,input enable,output reg out,output reg q); 
+   always @(*)
+   begin
+    case(enable)
+      1'b0:out=q;
+      1'b1:out=d;
+    endcase
+  end
+    always @ (posedge clk)
+     begin
+       q<=out;
+     end
+  endmodule
+  
+      
+    
