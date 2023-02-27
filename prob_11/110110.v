eb9e0906082a159c91aaac209751c4d3847ae73c
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-27 11:55:49 +0530

    Update 110110.v

--- prob_11/110110.v
+++ prob_11/110110.v
@@ -1,80 +1,36 @@
-module sequence_decetor_110110(input  clk,
-                   input  reset,
-                   input  din,
-                   output reg dout);
-reg [2:0] state;
-parameter S0 = 3'b000,
-          S1 = 3'b001,
-          S2 = 3'b010,
-          S3 = 3'b011,
-          S4 = 3'b100,
-          S5 = 3'b001;
+module sequence_detector_mealy(
+    input clk, rst, 
+    input din,      
+    output reg dout 
+);
 
-  always @(posedge clk or reset) begin
-    if(reset) begin
-      dout <= 1'b0;
-      state <= S0;
+reg [2:0] state; 
+parameter S0 = 3'd0, S1 = 3'd1, S2 = 3'd2, S3 = 3'd3, S4 = 3'd4, S5 = 3'd5;
+
+always @(posedge clk, posedge rst)
+begin
+    if (rst)
+        state <= S0; 
+    else
+    begin
+        case (state)
+            S0: if (din == 1) state <= S1; else state <= S0;
+            S1: if (din == 1) state <= S2; else state <= S0;
+            S2: if (din == 0) state <= S3; else state <= S0;
+            S3: if (din == 1) state <= S4; else state <= S0;
+            S4: if (din == 1) state <= S5; else state <= S2;
+            S5: if (din == 0) state <= S3; else state <= S0;
+            default: state <= S0; 
+        endcase
     end
-    else begin
-      case(state)
-        S0: begin
-          if(din) begin
-            state <= S1;
-            dout <=1'b0;
-          end
-          else
-            dout <=1'b0;
-        end
-        S1: begin
-          if(din) begin
-            state <= S2;
-            dout <=1'b0;
-          end
-          else begin
-            state <= S0;
-            dout <=1'b0;
-          end
-        end
-        S2: begin
-          if(~din) begin
-            state <= S3;
-            dout <=1'b0;
-          end
-          else begin
-            dout <=1'b0;
-          end
-        end
-        S3: begin
-          if(din) begin
-            state <= S4;
-            dout <=1'b0;
-          end
-          else begin
-            state <= S0;
-            dout <=1'b0;
-          end
-        end
-        S4: begin
-          if(din) begin
-            state <= S2;
-            dout <=1'b0;
-          end
-          else begin
-            state <= S0;
-            dout <=1'b0;
-          end
-        end
-        S5: begin
-        if (din) begin
-        state <= S4;
-        dout <=1'b0;
-        end
-        else begin
-        state <= S3;
-        dout <= 1'b1;
-        end end 
-      endcase
-    end
-  end
+end
+
+always @(state, din)
+begin
+    case (state)
+        S5: dout = 1; 
+        default: dout = 0;
+    endcase
+end
 
 endmodule
